085804dc98160a81c46261070aef99e5bbb2784d
LottoBKR <leonardobueras247@gmail.com>
2023-12-20 12:14:48 -0300

    Primer Commit

---
+++ src/main/java/com/bkr/blackbelt/BlackbeltApplication.java
@@ -0,0 +1,13 @@
+package com.bkr.blackbelt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackbeltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackbeltApplication.class, args);
+	}
+
+}
